06adf5942b2701e8846e81e43b689f426b4dc50d
Jeong Dong Won <chorok446@gmail.com>
2023-03-14 13:38:08 +0900

    feat: user 라우터 설정

--- src/app.js
+++ src/app.js
@@ -6,12 +6,12 @@ import commonErrors from './middlewares/filter/error/commonError';
 import { CustomError, httpExceptionFilter } from './middlewares/filter';
 import morganMiddleware from './middlewares/logger/morganMiddleware';
 import { logger } from './middlewares/logger/config/logger';
-import connection from './config/db.config';
+import { userController } from './controllers/user.controller';
 
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3000', // 허락하고자 하는 요청 주소
+  origin: 'http://localhost:3000', // 허락하고자 하는 요청 주소
 };
 
 const morganOption = ':method :status :url :response-time ms ip: :remote-addr';
@@ -21,29 +21,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 if (process.env.NODE_ENV === 'development') {
-    app.use(
-        morgan(morganOption, {
-            stream: logger.stream,
-        }),
-    );
+  app.use(
+    morgan(morganOption, {
+      stream: logger.stream,
+    }),
+  );
 } else if (process.env.NODE_ENV === 'production') {
-    app.use(morganMiddleware);
+  app.use(morganMiddleware);
 } else {
-    throw new CustomError(500, commonErrors.configError);
+  throw new CustomError(500, commonErrors.configError);
 }
+
+app.use('/api/user', userController);
 
 // 404 에러 핸들러
 app.use((req, res, next) => {
-    throw new CustomError(404, commonErrors.resourceNotFoundError);
+  throw new CustomError(404, commonErrors.resourceNotFoundError);
 });
 
 app.use(httpExceptionFilter);
-
-connection.connect((err) => {
-    if (err) {
-        throw new CustomError(500, commonErrors.dbConnectionError);
-    }
-    logger.info('DB 연결 성공');
-});
 
 export { app };
