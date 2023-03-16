69dae935debf9e4c3e22f5ff51a14d2983c5c366
Jeong Dong Won <chorok446@gmail.com>
2023-03-16 14:48:22 +0900

    refactor: 페이지네이션 구현 및 DB 테이블 변경으로 인한 메소드 리팩토링

--- src/services/facility.service.js
+++ src/services/facility.service.js
@@ -7,33 +7,21 @@ class FacilityService {
     this.facility = facilityRepository;
   }
 
-  // 문화시설 조회
-  async getFacilities() {
-    const facilities = await facilityRepository.getAll();
+  // 문화시설 조회 (페이징)
+  async findAll(pageSize, offset) {
+    const facilities = await facilityRepository.getAll(pageSize, offset);
 
     return facilities;
   }
 
   // 특정 문화시설 조회
-  async getFacilityById(facility_id) {
-    const facility = await facilityRepository.findOneById(facility_id);
+  async findOneById(facility_id) {
+    const facility = await facilityRepository.findById(facility_id);
 
     if (!facility) {
       throw new CustomError(404, commonErrors.resourceNotFoundError);
     }
-
-    return facility;
-  }
-
-  // 카테고리 별 문화시설 조회
-  async getFacilitiesByCategory(category_id) {
-    const facilities = await facilityRepository.findByCategory(category_id);
-
-    if (!facilities) {
-      throw new CustomError(404, commonErrors.resourceNotFoundError);
-    }
-
-    return facilities;
+    return facility[0];
   }
 }
 
