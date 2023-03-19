d3edba9b746143347625eb22111da17a3462d9ee
Jeong Dong Won <chorok446@gmail.com>
2023-03-19 18:24:49 +0900

    refactor: 문화시설 기능 리팩토링 및 전체 갯수 불러오는 기능 추가

--- src/db/repositories/Facility.js
+++ src/db/repositories/Facility.js
@@ -1,22 +1,23 @@
 import { execute } from '../../config/db.config';
 
 export class FacilityRepository {
-  reqponseData = `facility_id, subjcode, fac_name, district, addr, x_coord, y_coord, phne, homepage`;
+  responseData = `facility_id, subjcode, fac_name, district, addr, x_coord, y_coord, phne, homepage`;
 
   // 전체 문화시설 조회
   async getAll(pageSize, offset) {
-    const sql = `SELECT ${this.reqponseData} FROM Facility LIMIT ${pageSize} OFFSET ${offset}`;
+    const sql = `SELECT ${this.responseData} FROM Facility LIMIT ${pageSize} OFFSET ${offset}`;
     const max = `SELECT COUNT(*) FROM Facility`;
 
-    const result = await execute(sql);
-    const maxResult = await execute(max);
+    const [result, maxResult] = await Promise.all([execute(sql), execute(max)]);
+    const maxPage = Math.ceil(maxResult[0]['COUNT(*)'] / pageSize);
+    const totalCount = maxResult[0]['COUNT(*)'];
 
-    return { result, maxResult };
+    return { result, maxPage, totalCount };
   }
 
   // 문화시설 조회 (시설 이름 검색)
   async findBySearch(query) {
-    const sql = `SELECT ${this.reqponseData} FROM Facility WHERE fac_name LIKE "%${query}%"`;
+    const sql = `SELECT ${this.responseData} FROM Facility WHERE fac_name LIKE "%${query}%"`;
 
     return execute(sql);
   }
@@ -26,11 +27,11 @@ export class FacilityRepository {
     let sql;
 
     if (district === '전체' && subjcode === '전체') {
-      sql = `SELECT ${this.reqponseData} FROM Facility`;
+      sql = `SELECT ${this.responseData} FROM Facility`;
     } else if (district === '전체' || subjcode === '전체') {
-      sql = `SELECT ${this.reqponseData} FROM Facility WHERE district="${district}" OR subjcode="${subjcode}"`;
+      sql = `SELECT ${this.responseData} FROM Facility WHERE district="${district}" OR subjcode="${subjcode}"`;
     } else {
-      sql = `SELECT ${this.reqponseData} FROM Facility WHERE district="${district}" AND subjcode="${subjcode}"`;
+      sql = `SELECT ${this.responseData} FROM Facility WHERE district="${district}" AND subjcode="${subjcode}"`;
     }
 
     return execute(sql);
