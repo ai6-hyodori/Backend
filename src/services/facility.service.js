5a03f61b2fd6792b1c1097e999a0056285a90bfa
Jeong Dong Won <chorok446@gmail.com>
2023-03-19 18:24:08 +0900

    refactor: 누락된 코드 수정

--- src/services/facility.service.js
+++ src/services/facility.service.js
@@ -9,30 +9,27 @@ class FacilityService {
 
   // 문화시설 조회 (페이징)
   async findAll(pageSize, offset) {
-    const facilities = await facilityRepository.getAll(pageSize, offset);
+    const facilities = await this.facility.getAll(pageSize, offset);
 
     return facilities;
   }
 
   // 문화시설 조회 (시설 이름 검색)
   async findBySearch(query) {
-    const facilities = await facilityRepository.findBySearch(query);
+    const facilities = await this.facility.findBySearch(query);
 
     return facilities;
   }
 
   // 문화시설 조회 (자치구,  주제분류 필터링)
   async findByFilter(district, subjcode) {
-    const facilities = await facilityRepository.findByFilter(
-      district,
-      subjcode,
-    );
+    const facilities = await this.facility.findByFilter(district, subjcode);
     return facilities;
   }
 
   // 특정 문화시설 조회
   async findOneById(facility_id) {
-    const facility = await facilityRepository.findById(facility_id);
+    const facility = await this.facility.findById(facility_id);
 
     if (!facility) {
       throw new CustomError(404, commonErrors.resourceNotFoundError);
